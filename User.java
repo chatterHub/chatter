0656e56b5bfa6a6149a1880a5832976d078259ff
Collen Roller <rollercw195@whiteface.(none)>
2013-03-07 23:35:30 -0500

    added accessor methods to user class... and read the users file in the constructor... going to work on mutator methods next!

--- User.java
+++ User.java
@@ -4,61 +4,60 @@ import java.io.*;
 public class User {
 
     private String Username;
+    private String Password;
+    private String Email;
     private int level;
     private int points;
     private int qcorrect;
     private int qincorrect;
     private int totalquestions;
+    public boolean loggedIn;
     
-    public User(){
-        level = 1;
-        points = 0;
-        qcorrect = 0;
-        qincorrect = 0;
-        totalquestions = 0;
-    }
-    
-    public User(String u){
+    public User(String u) throws FileNotFoundException{
         Username = u;
+        loggedIn = true;
+        Scanner sc = new Scanner(new File("Users/" + Username + ".txt"));
+        while(sc.hasNext()){
+            String text = sc.next();
+            if(text.equals("Password:"))
+                Password = sc.next();
+            if(text.equals("E-mail:"))
+                Email = sc.next();
+            if(text.equals("Level:"))
+                level = sc.nextInt();
+            if(text.equals("Points:"))
+                points = sc.nextInt();
+            if(text.equals("Correct:"))
+                qcorrect = sc.nextInt();
+            if(text.equals("Incorrect:"))
+                qincorrect = sc.nextInt();
+            if(text.equals("Questions:"))
+                totalquestions = sc.nextInt();
+        }
+        System.out.println("User information stored");
     }
-
-    public String getEmail() throws FileNotFoundException{
-    	  Scanner input = new Scanner(new File("Users/" + Username + ".txt"));
-        String text = "";
-        while(input.hasNext()){
-           text += input.next() + " ";
-        }
-        //System.out.println(text);
-        String email = "";
-        int temp = 0;
-        for(int i=0; i<text.length(); i++){
-            if(text.substring(i,i+8).equals("E-mail: ")){
-                temp = i+8;
-            }
-            if(i+8>=text.length())
-            	break;
-        }
-        email += text.substring(temp,text.length());
-        return email;
+    //Accessor methods for user information
+    public String getPassword(){
+        return Password;
 	}
-	
-	//get level is not working
-	public int getLevel() throws FileNotFoundException{
-	  Scanner input = new Scanner(new File("Users/" + Username + ".txt"));
-        String text = "";
-        int i = 0;
-        int l = 0;
-        while(input.hasNext()){
-           text += input.next() + " ";
-           //System.out.println(text.substring(i,i+6));
-           if(text.substring(i,i+6).equals("Level:")){
-              System.out.println(text.substring(i,i+6));	
-           	  l = input.nextInt();
-           }
-           i++;
-        }
-        return l;
+    public String getEmail(){
+        return Email;
 	}
+	public int getLevel(){
+	    return level;
+	}
+	public int getPoints(){
+	    return points;
+	}
+	public int getCorrect(){
+	    return qcorrect;
+	}
+	public int getIncorrect(){
+	    return qincorrect;
+	}
+	public int getTotalQuestions(){
+	    return totalquestions;
+	}	
 }
 
 
--- test.java
+++ test.java
@@ -16,5 +16,11 @@ public class test {
         System.out.println("Collen's email: " + u.getEmail());
         //get level isn't working correctly
         System.out.println("Collen's level: " + u.getLevel());
+        System.out.println("Collen's points: " + u.getPoints());
+        System.out.println("Collen's has answered: " + u.getCorrect() +" correct");
+        System.out.println("Collen's has answered: " + u.getIncorrect() +" incorrect");
+        System.out.println("Collen's has answered: " + u.getTotalQuestions() +" total questions");
+        System.out.println("Collen's password is: " + u.getPassword());
+        
     }
 }
